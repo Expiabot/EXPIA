ecfbf2e37f17cd03a68a258bebe94532b4bc76d7
Expiabot <stbnmarteil@gmail.com>
2026-05-21 14:40:50 +0200

    Delete get_url.py

--- get_url.py
+++
@@ -1,6 +0,0 @@
-import urllib.request as u
-html = u.urlopen('https://my.spline.design/reactiveorb-LFaygHcVbYHKrJxdQxQSpzO9/').read().decode()
-for part in html.replace("'", '"').split('"'):
-    if "scene.splinecode" in part:
-        print(part)
-        break
